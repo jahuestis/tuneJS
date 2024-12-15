d2f1dab355cfe7edd74f4c20e71be202968082a7
jahuestis <160439894+jahuestis@users.noreply.github.com>
2024-12-14 19:02:34 -0500

    Multi client handling, private channels, improved interface

--- server/index.js
+++ server/index.js
@@ -1,10 +1,71 @@
-const ws = require('ws')
-const server = new ws.Server({ port: '3000' })
+const WebSocketServer = require('ws').Server;
+const socket = new WebSocketServer({ 
+    port: 3000, 
+});
+var clients = new Map();
 
-server.on('connection', socket => {
-    socket.on('message', message => {
-        const b = Buffer.from(message);
-        console.log(b.toString());
-        socket.send(`${message}`);
+socket.on('connection', (ws) => {
+    clients.set(ws, 0);
+    console.log('Client connected');
+
+    ws.on('message', (message) => {
+        try {
+            const messageStr = message instanceof Buffer ? message.toString() : message;
+            const data = JSON.parse(messageStr);
+            const targetChannel = data.channel;
+            const messageType  = data.type;
+            const messageText = data.text;
+            const messageColor = data.color;
+            // Verify validity of message
+            if (!(Number.isInteger(targetChannel) && targetChannel >= 0 && targetChannel <= 1000)) {
+                throw new Error(`Invalid message: ${targetChannel}`);
+            }
+            if (typeof messageText !== 'string') {
+                throw new Error(`Invalid message text: ${messageText}`);
+            }
+            if (typeof messageColor !== 'string') {
+                throw new Error(`Invalid message color: ${messageColor}`);
+            }
+            // Process message
+            if (messageType === 'textMessage') {
+                clients.forEach((channel, client) => {
+                    if (channel === targetChannel) {
+                        try {
+                            client.send(JSON.stringify(data));
+                        } catch (error) {
+                            console.log(`Unable to send message to client ${client}`);
+                            console.error(error);
+                        }
+                    }
+                });
+
+            } else if (messageType === 'updateChannel') {
+                clients.set(ws, targetChannel);
+                
+            } else {
+                throw new Error(`Invalid message type: ${messageType}`);
+            }
+        } catch (error) {
+            console.error(error);
+            ws.send(createMessage('error', 0, "We weren't able to process that :(", 'red'));
+        }
+    });
+
+    ws.on('close', () => {
+        clients.delete(ws);
+        console.log('Client disconnected');
+    });
+
+    ws.on('error', (error) => {
+        console.error(`Error on channel ${clients.get(ws)}: ${error.message}`);
+    });
+});
+
+function createMessage(type, channel=0, text='placeholder', color='lime') {
+    return JSON.stringify({
+        type: type,
+        channel: channel,
+        text: text,
+        color: color
     })
-})
+}
